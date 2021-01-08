7b22dcb80e6379b53ac201de0859b9084e66d638
Mikhail Velko <mikeveko@gmail.com>
2021-01-08 17:13:43 +0100

    server errors handling, step 78 next

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,3 +1,4 @@
+using System;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Authorization;
@@ -15,28 +16,35 @@ namespace API.Controllers
 
         [Authorize]
         [HttpGet("auth")]
-        public ActionResult<string> GetSecret(){
+        public ActionResult<string> GetSecret()
+        {
             return "secret text";
         }
 
         [HttpGet("not-found")]
-        public ActionResult<AppUser> GetNotFound(){
+        public ActionResult<AppUser> GetNotFound()
+        {
             var thing = _context.Users.Find(-1);
-            if(thing == null) return NotFound();
+            if (thing == null) return NotFound();
             return Ok(thing);
         }
 
         [HttpGet("server-error")]
-        public ActionResult<string> GetSErverError(){
+        public ActionResult<string> GetServerError()
+        {
+
             var thing = _context.Users.Find(-1);
 
             var thingToReturn = thing.ToString();
 
             return thingToReturn;
+
+
         }
 
         [HttpGet("bad-request")]
-        public ActionResult<string> GetBadRequest(){
+        public ActionResult<string> GetBadRequest()
+        {
             return BadRequest("This was not a good request");
         }
     }
